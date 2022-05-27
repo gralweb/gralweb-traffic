a9131030642442d774a4106553693222152c4449
gralweb <gralweb21@gmail.com>
2022-05-27 00:15:11 -0400

    script de prueba para generar trafico

---
+++ src/js/index.js
@@ -0,0 +1,44 @@
+const _global = ({_url, _counter, _timer_prop}) => {
+	let _window, _interval, _timer = _timer_prop, _counter_open = 0;
+	let _counter_show = document.querySelectorAll("._counter")[_counter]
+
+	const _handle_open = () => {
+		_window = window.open(_url, '_blank')
+		_counter_open++
+	}
+
+	const _handle_close = () => {
+		_window.close()
+		_counter_show.innerHTML = _counter_open
+	}
+
+	let _open_link = () => {
+
+		_handle_open()
+
+		_interval = setInterval( () => {
+			_timer--
+			if (_timer < 0) {
+				_timer = _timer_prop
+				_handle_close()
+				_reset_interval()
+				clearInterval(_interval)
+			}
+		}, 1000 )
+	}
+
+	let _reset_interval = () => {
+		if (_timer === _timer_prop) {
+			setTimeout(() => {
+				_open_link()
+			}, 5000)
+		}
+	}
+
+	_reset_interval()
+}
+
+window.onload = () => {
+	_global({ _url: 'http://adfoc.us/7616461', _counter: 0, _timer_prop: 20  })
+	_global({ _url: 'https://t.co/cccjHuie8O', _counter: 1, _timer_prop: 35  })
+}
